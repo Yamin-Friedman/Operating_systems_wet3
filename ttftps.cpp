b12f2257d344d55fc9e32682061af217270dd4a6
Yamin <yaminrf@gmail.com>
2019-01-04 10:02:15 +0200

    Continued work

--- ttftps.cpp
+++ ttftps.cpp
@@ -5,6 +5,10 @@
 #include <sys/socket.h>
 #include <winsock.h>
 #include <cstdio>
+#include <unistd.h>
+#include <vector>
+
+using namespace std;
 
 #define TEMP_PORT 69 // To be replaced
 #define WAIT_FOR_PACKET_TIMEOUT 3
@@ -58,7 +62,70 @@ int main(){
 			exit(-1);
 		}
 
+		char *buffer = new(516);
 
+		res = recv(clientfd,buffer,516,NULL);
+		if (res == -1) {
+			perror("TTFTP_ERROR:");
+			delete buffer;
+			exit(-1);
+		}
+
+		int size_read  = res;
+
+		res = check_WRQ_packet(buffer,size_read);
+		if (res == -1) {
+			//error with WRQ
+		}
+
+		send_ACK_packet(clientfd,0);
+
+		do
+		{
+			do
+			{
+
+				do
+				{
+					// TODO: Wait WAIT_FOR_PACKET_TIMEOUT to see if something appears
+					// for us at the socket (we are waiting for DATA)
+
+					if ()// TODO: if there was something at the socket and
+						// we are here not because of a timeout
+					{
+						// TODO: Read the DATA packet from the socket (at
+						// least we hope this is a DATA packet)
+					}
+					if (...) // TODO: Time out expired while waiting for data
+					// to appear at the socket
+					{
+//TODO: Send another ACK for the last packet
+						timeoutExpiredCount++;
+					}
+
+					if (timeoutExpiredCount>= NUMBER_OF_FAILURES)
+					{
+						// FATAL ERROR BAIL OUT
+					}
+
+				}while (...) // TODO: Continue while some socket was ready
+				// but recvfrom somehow failed to read the data
+
+				if (...) // TODO: We got something else but DATA
+				{
+					// FATAL ERROR BAIL OUT
+				}
+				if (...) // TODO: The incoming block number is not what we have
+				// expected, i.e. this is a DATA pkt but the block number
+				// in DATA was wrong (not last ACK’s block number + 1)
+				{
+					// FATAL ERROR BAIL OUT
+				}
+			}while (FALSE);
+			timeoutExpiredCount = 0;
+			lastWriteSize = fwrite(...); // write next bulk of data
+// TODO: send ACK packet to the client
+		}while (...); // Have blocks left to be read from client (not end of transmission)
 	}
 
 
